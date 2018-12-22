b3ab9a9370f0810250466ba9589b918b27bf6400
Lynn Kirby <lynn@arrak.is>
2018-12-22 01:11:42 -0500

    [emacs] Setup chemacs with Doom and Spacemacs profiles

---
+++ emacs/.emacs-profiles.el
@@ -0,0 +1,4 @@
+(("doom"     . ((user-emacs-directory . "~/dotfiles/emacs/base/doom")
+                (env . (("DOOMDIR" . "~/dotfiles/emacs/config/doom")))))
+("spacemacs" . ((user-emacs-directory . "~/dotfiles/emacs/base/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/dotfiles/emacs/config/spacemacs"))))))
